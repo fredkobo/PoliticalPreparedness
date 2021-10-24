4dd7799f763772db582e0db3f518d4ff7b254acf
Frederick Kobo <frederick.kobo@ebucks.com>
2021-10-24 15:41:48 +0200

    Add headers to recyclerviews

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.database.ElectionDatabase
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
@@ -37,19 +38,23 @@ class ElectionsFragment : Fragment() {
 
         binding.viewModel = viewModel
 
-        val upcomingElectionsAdapter = ElectionListAdapter(ElectionListener { electionId ->
-            viewModel.onElectionClicked(electionId)
-        })
+        val upcomingElectionsAdapter = ElectionListAdapter(
+            getString(R.string.upcoming_elections),
+            ElectionListener { electionId ->
+                viewModel.onElectionClicked(electionId)
+            })
 
-        val savedElectionsAdapter = ElectionListAdapter(ElectionListener { electionId ->
-            viewModel.onElectionClicked(electionId)
-        })
+        val savedElectionsAdapter = ElectionListAdapter(
+            getString(R.string.saved_elections),
+            ElectionListener { electionId ->
+                viewModel.onElectionClicked(electionId)
+            })
 
         binding.upcomingElectionsRecycler.apply {
             adapter = upcomingElectionsAdapter
             viewModel.upcomingElections.observe(viewLifecycleOwner, Observer { upcomingElections ->
                 upcomingElections?.let {
-                    upcomingElectionsAdapter.submitList(upcomingElections)
+                    upcomingElectionsAdapter.addHeaderAndSubmitList(upcomingElections)
                 }
             })
         }
@@ -58,7 +63,7 @@ class ElectionsFragment : Fragment() {
             adapter = savedElectionsAdapter
             viewModel.savedElections.observe(viewLifecycleOwner, Observer { savedElections ->
                 savedElections?.let {
-                    savedElectionsAdapter.submitList(savedElections)
+                    savedElectionsAdapter.addHeaderAndSubmitList(savedElections)
                 }
             })
         }
--- app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/adapter/ElectionListAdapter.kt
@@ -5,52 +5,113 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.politicalpreparedness.databinding.ItemHeaderBinding
 import com.example.android.politicalpreparedness.databinding.ListItemElectionBinding
 import com.example.android.politicalpreparedness.network.models.Election
 
-class ElectionListAdapter(private val clickListener: ElectionListener): ListAdapter<Election, ElectionListAdapter.ElectionViewHolder>(ElectionDiffCallback()) {
+class ElectionListAdapter(
+    private val headerTitle: String,
+    private val clickListener: ElectionListener
+) : ListAdapter<DataItem, RecyclerView.ViewHolder>(DiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElectionViewHolder {
-        return ElectionViewHolder.from(parent)
+    private val TYPE_LIST_HEADER = 0
+    private val TYPE_LIST_ITEM = 1
+
+    fun addHeaderAndSubmitList(list: List<Election>?) {
+        val items = when (list) {
+            null -> listOf(DataItem.Header)
+            else -> listOf(DataItem.Header) + list.map { DataItem.ElectionItem(it) }
+        }
+        submitList(items)
     }
 
-    override fun onBindViewHolder(holder: ElectionViewHolder, position: Int) {
-        val item = getItem(position)
 
-        holder.bind(clickListener, item)
+    companion object DiffCallback : DiffUtil.ItemCallback<DataItem>() {
+        override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
+            return oldItem.id == newItem.id
+        }
     }
 
-    class ElectionViewHolder private constructor(val binding: ListItemElectionBinding) :
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_LIST_HEADER -> ViewHolderElectionHeader.from(parent)
+            TYPE_LIST_ITEM -> ViewHolderElectionBinding.from(parent)
+            else -> throw ClassCastException("Unknown viewType ${viewType}")
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> TYPE_LIST_HEADER
+            is DataItem.ElectionItem -> TYPE_LIST_ITEM
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ViewHolderElectionHeader -> {
+                holder.bind(headerTitle)
+            }
+            is ViewHolderElectionBinding -> {
+                holder.bind(
+                    (getItem(position) as DataItem.ElectionItem).election,
+                    clickListener
+                )
+            }
+        }
+    }
+
+    class ViewHolderElectionBinding(private var binding: ListItemElectionBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(clickListener: ElectionListener, item: Election) {
-            binding.election = item
+        fun bind(election: Election, clickListener: ElectionListener) {
+            binding.election = election
             binding.clickListener = clickListener
             binding.executePendingBindings()
         }
 
         companion object {
-            fun from(parent: ViewGroup): ElectionViewHolder {
+            fun from(parent: ViewGroup): ViewHolderElectionBinding {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ListItemElectionBinding.inflate(layoutInflater, parent, false)
+                return ViewHolderElectionBinding(binding)
+            }
+        }
+    }
 
-                return ElectionViewHolder(binding)
+    class ViewHolderElectionHeader(private var binding: ItemHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(title: String) {
+            binding.title = title
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolderElectionHeader {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemHeaderBinding.inflate(layoutInflater, parent, false)
+                return ViewHolderElectionHeader(binding)
             }
         }
     }
 }
 
-class ElectionDiffCallback : DiffUtil.ItemCallback<Election>() {
-    override fun areItemsTheSame(oldItem: Election, newItem: Election): Boolean {
-        return oldItem.id == newItem.id
+sealed class DataItem {
+    data class ElectionItem(val election: Election) : DataItem() {
+        override val id = election.id
     }
 
-    override fun areContentsTheSame(oldItem: Election, newItem: Election): Boolean {
-        return oldItem == newItem
+    object Header : DataItem() {
+        override val id = Int.MIN_VALUE
     }
+
+    abstract val id: Int
 }
 
-//TODO: Create ElectionListener
 class ElectionListener(val clickListener: (election: Election) -> Unit) {
     fun onClick(election: Election) = clickListener(election)
 }
